627d5f5e2c69d0a29f78c22ff53d6fd3584ba8ba
Denis Eiras <denis.eiras@gmail.com>
2024-02-23 16:36:20 -0300

    #435 - install and test

---
+++ 435/install.bash
@@ -0,0 +1,4 @@
+module load python-3.9.15-gcc-9.4.0-f466wuv
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
---
+++ 435/test.sh
@@ -0,0 +1,3 @@
+# cd /mnt/beegfs/denis.eiras/MONAN_GCC_issues/435
+time python isobaric_interp.py /mnt/beegfs/monan/MONAN-scripts/egeon_oper/MONAN/testcase/runs/GFS/2024022000/monanprd/history.2024-03-01_00.00.00.nc iso_winds.nc > out.txt
+
