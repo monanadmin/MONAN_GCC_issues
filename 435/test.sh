fc7e22b70efeed73852c7338ab74b5a93608b047
Denis Eiras <denis.eiras@gmail.com>
2024-03-08 17:14:00 -0300

    35 - test.sh

--- 435/test.sh
+++ 435/test.sh
@@ -1,4 +1,4 @@
 # cd /mnt/beegfs/denis.eiras/MONAN_GCC_issues/435
 current_date=$(date +'%Y%m%d%H%M%S')
-time python isobaric_interp.py /mnt/beegfs/monan/MONAN-scripts/egeon_oper/MONAN/testcase/runs/GFS/2024022000/monanprd/history.2024-03-01_00.00.00.nc "iso_winds_${current_date}.nc" > out_serial.txt
+time python isobaric_interp.py /mnt/beegfs/monan/MONAN-scripts/egeon_oper/MONAN/testcase/runs/GFS/2024022000/monanprd/history.2024-03-01_00.00.00.nc "iso_winds_${current_date}.nc" > out_parallel_np.txt
 
