ee7993f20fe3cd201ab8959cdd19332742ebed6e
Denis Eiras <denis.eiras@gmail.com>
2024-05-09 12:10:52 -0300

    #500 passing params

--- monan-500-NF/group_levels.py
+++ monan-500-NF/group_levels.py
@@ -1,17 +1,26 @@
 import netCDF4 as nc
 import numpy as np
 import re
+import sys
 
-# Open the NetCDF file
-data_dir = '/mnt/beegfs/denis.eiras/monan_github_issues_workspace/monan-500/'
-# nc_file = nc.Dataset(f'{data_dir}diagnostics_2024-02-01.nc', 'r')
-nc_file = nc.Dataset(f'{data_dir}MONAN_DIAG_G_POS_GFS_2024042900_2024042900.00.00.x1024002L55.nc', 'r')
+if len(sys.argv) != 4:
+    print("Usage: python script.py <string1> <string2>")
+    sys.exit(1)
+
+# /mnt/beegfs/eduardo.khamis/issues/488/feature-488/scripts_CD-CT/dataout/2024020100/Post
+data_dir = sys.argv[1]
+file_in = sys.argv[2]
+file_out = sys.argv[3]
+
+nc_file_in = nc.Dataset(f'{data_dir}/{file_in}', 'r')
+# Save the concatenated variables to a new NetCDF file
+nc_file_out = nc.Dataset(f'{data_dir}/{file_out}', 'w')
 
 dimensions_4D = ('Time', 'latitude', 'longitude', 'level')
 # Group variables based on their names
 variable_groups = {}
 first_hpa_variable = ''
-for name, variable in nc_file.variables.items():
+for name, variable in nc_file_in.variables.items():
     # if 'hPa' in name or name in dimensions_4D:
     if 'hPa' in name:
         variable_type = re.split('_.*hPa', name)[0]  # Extract the variable type (e.g., '15hPa', '20hPa')
@@ -26,34 +35,32 @@ for name, variable in nc_file.variables.items():
 
 level_dimension_size = len(variable_groups[first_hpa_variable]) 
 
-# Save the concatenated variables to a new NetCDF file
-new_nc_file = nc.Dataset(f'{data_dir}concatenated_file.nc', 'w')
 
 # Copy dimensions Time, latitude, longitude
-for dim_name, dim_type in nc_file.dimensions.items():
+for dim_name, dim_type in nc_file_in.dimensions.items():
     if dim_name in ['Time', 'latitude', 'longitude']:
-        new_nc_file.createDimension(dim_name, dim_type.size)
+        nc_file_out.createDimension(dim_name, dim_type.size)
 # Create level dimension
-new_nc_file.createDimension('level', level_dimension_size)
+nc_file_out.createDimension('level', level_dimension_size)
 
 # Copy variables
 for variable_type, variables in variable_groups.items():
     print(f'Creating var {variable_type}')
     if variable_type == 'level':
-        new_variable = new_nc_file.createVariable(variable_type, variables[0].dtype, variables[0].dimensions)
+        new_variable = nc_file_out.createVariable(variable_type, variables[0].dtype, variables[0].dimensions)
         new_variable.setncatts({k: variables[0].getncattr(k) for k in variables[0].ncattrs()})
         new_variable[:] = range(level_dimension_size, 0, -1)
     elif len(variables) == level_dimension_size:
-        new_variable = new_nc_file.createVariable(variable_type, variables[0].dtype, dimensions_4D)
+        new_variable = nc_file_out.createVariable(variable_type, variables[0].dtype, dimensions_4D)
         new_variable.setncatts({k: variables[0].getncattr(k) for k in variables[0].ncattrs()})
         for i in range(level_dimension_size):
             print(f'copying variable level {i}')
             new_variable[:,:,:,i] = variables[i][:]
     else:
         # variable_type in ('latitude', 'longitude', 'Time'):
-        new_variable = new_nc_file.createVariable(variable_type, variables[0].dtype, variables[0].dimensions)
+        new_variable = nc_file_out.createVariable(variable_type, variables[0].dtype, variables[0].dimensions)
         new_variable.setncatts({k: variables[0].getncattr(k) for k in variables[0].ncattrs()})
         new_variable[:] = variables[0][:]
 
-new_nc_file.close()
-nc_file.close()
+nc_file_out.close()
+nc_file_in.close()
